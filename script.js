fe44202b9b8455e2f59dea2bc1d4a6c0262433cb
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-10-20 23:47:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const name=document.getElementById(fname);
+ name.addEventListener('blur', function() {
+	   nameInput.value = nameInput.value.toUpperCase();
+        });
